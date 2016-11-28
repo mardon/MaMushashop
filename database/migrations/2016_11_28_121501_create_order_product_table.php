6e7a8a69a70f1b1085f739ba73dc8d022bbe9132
MaMusha <mar.don@seznam.cz>
2016-11-28 15:01:25 +0100

    First

---
+++ database/migrations/2016_11_28_121501_create_order_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->integer('order_id')->unsigned()->index();
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->integer('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->integer('quantity');
+            $table->decimal('price',10,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('order_product');
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# MaMushashop
+
+MaMushashop is small e-shop web application with Laravel framework.
+It is made by MaMusha, which is learning Laravel. 
+
+## License
+
+The MaMushashop is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
