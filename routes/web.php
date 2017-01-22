284150496230b531cec261f85bd2b038a5f0da77
cesar\mardon <mar.don@seznam.cz>
2017-01-22 21:19:49 +0100

    6th

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,17 @@ Route::get('/cart','CartController@index');
 
 Route::get('/checkout','CheckoutController@index');
 Route::post('/checkout', 'CheckoutController@add');
+
+Route::group(['middleware' => ['web']], function() {
+    Route::get('login', ['as' => 'login', 'uses' => 'Auth\LoginController@showLoginForm']);
+    Route::post('login', ['as' => 'login.post', 'uses' => 'Auth\LoginController@login']);
+    Route::get('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
+
+    Route::get('password/reset', ['as' => 'password.reset', 'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm']);
+    Route::post('password/email', ['as' => 'password.email', 'uses' => 'Auth\ForgotPasswordController@sendResetLinkEmail']);
+    Route::get('password/reset/{token}', ['as' => 'password.reset.token', 'uses' => 'Auth\ResetPasswordController@showResetForm']);
+    Route::post('password/reset', ['as' => 'password.reset.post', 'uses' => 'Auth\ResetPasswordController@reset']);
+});
+Route::group(['prefix' => 'admin',  'middleware' => 'admin'],function () {
+    Route::get('/', 'AdminController@index');
+});
