8891f14aea7856096ba6dd6eea2fc85f69b66a05
MarDon <mar.don@seznam.cz>
2017-03-28 09:59:34 +0200

    Display modal after add to cart

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,13 +26,31 @@
                                 <input type="hidden" name="product_id" value="{{ $product->id }}">
                                 {{--<button type="submit"  class="btn btn-primary">Koupit</button>--}}
                             </form>
-                            <a href="#" id="add" class="btn btn-primary" data-id="{{ $product->id }}">Koupit</a>
+                            <a href="#" id="add" class="btn btn-primary" data-id="{{ $product->id }}" data-product="{{ $product->name }}" data-price="{{ $product->price }}">Koupit</a>
                         </div>
                     </div>
                 @endforeach
             </div>
         @endforeach
     </div>
+    <div class="modal fade" tabindex="-1" role="dialog" id="add-modal">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Zboží přidáno do košíku</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="modal-product"></div>
+                    <div class="modal-price"></div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Pokračovat v nákupu</button>
+                    <a href="{{ URL::to('cart') }}" type="button" class="btn btn-primary">Objednat</a>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 @stop
 
 @section('scripts')
@@ -47,21 +65,26 @@
         $('a#add').click( function() {
             var product_id = $(this).data('id');
             var url = "/ajaxadd";
+            var product_name = $(this).data('product');
+            var product_price = $(this).data('price');
 
             $.ajax({
 
-            type: "POST",
-            url: url,
-            data: { product_id: product_id },
-            success: function (msg) {
-            console.log(JSON.stringify(msg));
-            var obj = JSON.parse(JSON.stringify(msg));
-            $('.badge').html(obj.msg);
+                type: "POST",
+                url: url,
+                data: { product_id: product_id },
+                success: function (msg) {
+                console.log(JSON.stringify(msg));
+                var obj = JSON.parse(JSON.stringify(msg));
+                $('.badge').html(obj.msg);
+                $('#add-modal .modal-product').html(product_name);
+                $('#add-modal .modal-price').html(product_price);
+                $('#add-modal').modal();
 
-            },
-            error: function (data) {
-            console.log('Error:', data);
-            }
+                },
+                error: function (data) {
+                console.log('Error:', data);
+                }
             });
         });
     });
